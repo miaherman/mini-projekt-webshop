e14f4be89f6aaac05439a18a2e1cdcb2b43df984
miaherman <mia.herman@live.se>
2021-03-10 14:46:56 +0100

    formulärfix

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,22 +1,120 @@
 import React, { Component } from "react";
 import ButtonAppBar from "./buttonAppBar";
+import { makeStyles } from '@material-ui/core/styles';
 
 import { FormControl } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 
-export default class Checkout extends Component {
-  
+  const useStyles = makeStyles((theme: any) => ({
+      root: {
+        justifyContent: 'center',
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '100%',
+      },
+    })
+  );
+export default function Checkout() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+        <><ButtonAppBar/></>
+      <div>
+        <TextField
+          label="Normal"
+          id="margin-normal"
+          defaultValue="Default Value"
+          className={classes.textField}
+          helperText="Some important text"
+          margin="normal"
+        />
 
-    render() {
-      return (
-        <>
-         <ButtonAppBar/>
-           <FormControl>
-                <TextField id="standard-basic" label="Mitt lilleformulärhehe" />
-           </FormControl>
-        </>
+        <TextField
+          id="filled-full-width"
+          label="Label"
+          style={{ margin: 8 }}
+          placeholder="Placeholder"
+          helperText="Full width!"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="filled"
+        />
+        <TextField
+          label="None"
+          id="filled-margin-none"
+          defaultValue="Default Value"
+          className={classes.textField}
+          helperText="Some important text"
+          variant="filled"
+        />
+        <TextField
+          label="Dense"
+          id="filled-margin-dense"
+          defaultValue="Default Value"
+          className={classes.textField}
+          helperText="Some important text"
+          margin="dense"
+          variant="filled"
+        />
+        <TextField
+          label="Normal"
+          id="filled-margin-normal"
+          defaultValue="Default Value"
+          className={classes.textField}
+          helperText="Some important text"
+          margin="normal"
+          variant="filled"
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-full-width"
+          label="Label"
+          style={{ margin: 8 }}
+          placeholder="Placeholder"
+          helperText="Full width!"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        />
+        <TextField
+          label="None"
+          id="outlined-margin-none"
+          defaultValue="Default Value"
+          className={classes.textField}
+          helperText="Some important text"
+          variant="outlined"
+        />
+        <TextField
+          label="Dense"
+          id="outlined-margin-dense"
+          defaultValue="Default Value"
+          className={classes.textField}
+          helperText="Some important text"
+          margin="dense"
+          variant="outlined"
+        />
+        <TextField
+          label="Normal"
+          id="outlined-margin-normal"
+          defaultValue="Default Value"
+          className={classes.textField}
+          helperText="Some important text"
+          margin="normal"
+          variant="outlined"
+        />
+      </div>
 
-      
-      );
-    }
+      </div>
+    );
   }
